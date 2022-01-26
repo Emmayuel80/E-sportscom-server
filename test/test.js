f404a4ed698ec31aa68eb5a3bcabd94f32648e5b
EduardoCedillo <eduartcedillo@gmail.com>
2022-01-25 18:04:49 -0600

    se agrego la ejecucion del test

--- test/test.js
+++ test/test.js
@@ -1,4 +1,9 @@
+/* eslint-disable no-undef */
+// Sample test
 function sum(a, b) {
   return a + b;
 }
-module.exports = sum;
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(sum(1, 2)).toBe(3);
+});
